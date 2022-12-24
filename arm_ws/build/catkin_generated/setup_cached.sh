3278e15d5668db46e086a3333a3de72e04e0bf95
srrcanpolat <srrcan.polat@gmail.com>
2022-12-24 19:07:42 +0300

    branch created

--- arm_ws/build/catkin_generated/setup_cached.sh
+++ arm_ws/build/catkin_generated/setup_cached.sh
@@ -7,6 +7,6 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH='/opt/ros/noetic/lib'
-export PWD='/home/scp/arm_project/arm_ws/build'
-export ROS_PACKAGE_PATH='/home/scp/arm_project/arm_ws/src:/opt/ros/noetic/share'
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu'
+export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig:/opt/ros/noetic/lib/x86_64-linux-gnu/pkgconfig'
+export PWD='/root/arm_project/arm_ws/build'
