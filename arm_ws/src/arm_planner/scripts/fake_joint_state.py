047c2456cb5f4c4a0fa6d5bf2d8583f29efa57c3
srrcanpolat <srrcan.polat@gmail.com>
2023-03-14 14:11:32 +0300

    added physical servo publishers

--- arm_ws/src/arm_planner/scripts/fake_joint_state.py
+++ arm_ws/src/arm_planner/scripts/fake_joint_state.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Float64, String
+from std_msgs.msg import Float64, String, UInt16
 from sensor_msgs.msg import JointState
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
 
 def joint_callback(msg: JointState):
     #rate = rospy.Rate(100)
@@ -12,6 +24,9 @@ def joint_callback(msg: JointState):
     pub3.publish(dne[2])
     pub4.publish(dne[3])
     pub5.publish(dne[4])
+    pub6.publish(int(translate(dne[0],-1.570796,1.570796,0,180)))
+    pub7.publish(int(translate(dne[1],-1.570796,1.570796,0,180)))
+    pub8.publish(int(translate(dne[2],-1.570796,1.570796,0,180)))
     #rate.sleep()
 
 if __name__ == "__main__":
@@ -24,6 +39,9 @@ if __name__ == "__main__":
         pub3 = rospy.Publisher("/robot_arm/arm1_joint_position_controller/command", Float64, queue_size=10)
         pub4 = rospy.Publisher("/robot_arm/arm2_joint_position_controller/command", Float64, queue_size=10)
         pub5 = rospy.Publisher("/robot_arm/arm3_joint_position_controller/command", Float64, queue_size=10)
+        pub6 = rospy.Publisher("/f_servo/servo_base", UInt16, queue_size=10)
+        pub7 = rospy.Publisher("/f_servo/servo_waist", UInt16, queue_size=10)
+        pub8 = rospy.Publisher("/f_servo/servo_arm1", UInt16, queue_size=10)
 
         rospy.spin()
 
